cf2b5c0b2f2ab187b72f5843c7f7bf51dfbb1847
beverts312 <texas312@gmail.com>
2016-12-13 14:58:14 -0600

    add hello world frame

---
+++ generators/app/templates/express-api/src/app.ts
@@ -0,0 +1,18 @@
+import express = require('express');
+
+let app = express();
+
+//Add CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+//Sample Get Method
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World');
+});
+
+//Start Server on port 3000
+app.listen(3000);
