a991129966a99f306e9002bb4304389b0788072d
Bailey Everts <beverts312@users.noreply.github.com>
2016-12-16 10:04:31 -0600

    Completely Remove q (#19)
    * target es6, remove q since we can use native promises
    
    * remove dependency on q
    
    * remove unneeded async keyword

--- generators/app/templates/vsts-task/src/helper.ts
+++ generators/app/templates/vsts-task/src/helper.ts
@@ -1,7 +1,5 @@
 'use strict';
 
-import Q = require('q');
-
 /**
  * Example of a Helper class.
  * 
@@ -13,23 +11,18 @@ class Helper {
      * 
      * @param {number} a - The first input.
      * @param {number} b - The second input.
-     * @returns {Q.Promise<number>}
+     * @returns {Promise<number>}
      * 
      * @memberOf Helper
      */
-    public add(a: number, b: number): Q.Promise<number> {
-        const deferred: Q.Deferred<number> = Q.defer<number>();
-
-        if (isNaN(a) || isNaN(b)) {
-            return Q.reject<number>(new Error('Invalid inputs.'));
-            //deferred.reject(new Error());
-        } else {
-            var sum = a + b;
-            return Q.resolve<number>(sum);
-            //deferred.resolve(sum);
-        }
-
-        //return deferred.promise;
+    public add(a: number, b: number): Promise<number> {
+        return new Promise<number>((resolve, reject) => {
+            if (isNaN(a) || isNaN(b)) {
+                reject('Invalid inputs.');
+            } else {
+                resolve(a + b);
+            }
+        });
     }
 }
 
