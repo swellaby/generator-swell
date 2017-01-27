b4dcf8d4402aa7558bbc5ae040c1f4bbbda731fb
Caleb Cartwright <caleb.cartwright@outlook.com>
2017-01-26 22:21:54 -0600

    updated main.ts tests for vsts-task project

--- generators/app/templates/vsts-task/test/unit/main-tests.ts
+++ generators/app/templates/vsts-task/test/unit/main-tests.ts
@@ -2,6 +2,7 @@
 
 import Chai = require('chai');
 import Sinon = require('sinon');
+import tl = require('vsts-task-lib/task');
 
 import Helper = require('../../src/helper');
 import main = require('../../src/main');
@@ -9,5 +10,15 @@ import main = require('../../src/main');
 const assert = Chai.assert;
 
 suite('Main Suite: ', () => {
+    let sandbox: Sinon.SinonSandbox;
+    let taskLibGetInputStub: Sinon.SinonStub;
 
+    suiteSetup(() => {
+        sandbox = Sinon.sandbox.create();
+        taskLibGetInputStub = Sinon.stub(tl, 'getInput');
+    });
+
+    suiteTeardown(() => {
+        sandbox.restore();
+    });
 });
